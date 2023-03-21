87e3e4d94ffe3f7ee0234161f77001d6d00cbb8c
unknown <valdineiplimajunior@gmail.com>
2023-03-20 21:57:13 -0300

    kdjfskdsnfk

---
+++ Atividade.py
@@ -0,0 +1,15 @@
+# Atribuindo valores às variáveis
+nome = "valdinei pereira de lima junior"
+sobrenome = "Silva"
+idade = 19
+altura = 1.72
+peso = 58.8
+maior_idade = idade >= 18
+
+# Exibindo resultados
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade:", maior_idade)
